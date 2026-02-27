f19321f1d9dd65e58e73a95607e7d6b8ea173907
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-02-27 21:09:35 +0300

    add styles

--- script.js
+++ script.js
@@ -23,12 +23,12 @@ function GameState() {
   const player1 = {
     name: "Player X",
     score: 0,
-    token: "X",
+    token: "x",
   };
   const player2 = {
     name: "Player O",
     score: 0,
-    token: "O",
+    token: "o",
   };
   let tieCount = 0;
   let activeStatus = "playing"; // playing, win, tie
@@ -194,15 +194,21 @@ function GameView() {
   const scoreP2El = $(".score-p2 .count");
   const scoreTiesEl = $(".score-ties .count");
 
+  const getPlayerIcon = (token) => {
+    if (!token) return "";
+    return `<img src="/assets/icon-${token}.svg">`;
+  };
+
   function renderBoard(state) {
-    activePlayerEl.textContent = state.activePlayer.name;
+    activePlayerEl.innerHTML =
+      getPlayerIcon(state.activePlayer.token) + " TURN";
     boardEl.textContent = "";
 
     state.board.forEach((cell, index) => {
       const tableCell = document.createElement("button");
       tableCell.classList.add("cell");
       tableCell.dataset.cell = index;
-      tableCell.textContent = cell;
+      tableCell.innerHTML = getPlayerIcon(cell);
       boardEl.appendChild(tableCell);
     });
 
@@ -235,9 +241,10 @@ function GameView() {
     roundModalEl.showModal();
     if (state.status === "win") {
       roundModalWinnerEl.textContent = `${state.activePlayer.name} WINS!`;
-      roundModalResultEl.textContent = `${state.activePlayer.token} TAKES THE ROUND`;
+      roundModalResultEl.innerHTML = `${getPlayerIcon(state.activePlayer.token)} TAKES THE ROUND`;
+      roundModalResultEl.classList.add(`${state.activePlayer.token}-win`);
     } else if (state.status === "tie") {
-      roundModalWinnerEl.textContent = "";
+      roundModalWinnerEl.style.display = "none";
       roundModalResultEl.textContent = "ROUND TIED";
     }
   }
@@ -322,128 +329,3 @@ function printTableInConsole(board) {
 
   return row1 + border + row2 + border + row3;
 }
-
-// View
-/*
-function displayController(game, gameState) {
-  const $ = (selector) => document.querySelector(selector);
-
-  const activePlayerEl = $(".active-player");
-  const boardEl = $(".board");
-  const nameP1El = $(".stats .name-p1");
-  const nameP2El = $(".stats .name-p2");
-  const scoreP1El = $(".score-p1 .count");
-  const scoreP2El = $(".score-p2 .count");
-  const scoreTiesEl = $(".score-ties .count");
-
-  const roundModalEl = $(".round-modal");
-  const msgWinnerEl = $(".msg-winner");
-  const msgRoundEl = $(".msg-round");
-  const nextRoundBtn = $(".round-modal .affirm");
-  const quitBtn = $(".round-modal .negative");
-
-  const restartModalEl = $(".restart-modal");
-  const restartModalBtn = $(".restart");
-
-  const getPlayerIcon = (token) => {
-    if (!token) return "";
-    return `<img src="/assets/icon-${token.toLowerCase()}.svg">`;
-  };
-
-  function updateScreen() {
-    const board = game.getBoard();
-
-    boardEl.textContent = "";
-    activePlayerEl.innerHTML = `${getPlayerIcon(game.getActivePlayer().token)} TURN`;
-
-    board.forEach((cell, index) => {
-      const tableCell = document.createElement("button");
-      tableCell.classList.add("cell");
-      tableCell.dataset.cell = index;
-      tableCell.innerHTML = getPlayerIcon(cell.getValue());
-      boardEl.appendChild(tableCell);
-    });
-
-    nameP1El.textContent = gameState.getPlayerOneName();
-    nameP2El.textContent = gameState.getPlayerTwoName();
-    scoreP1El.textContent = gameState.getPlayerOneScore();
-    scoreP2El.textContent = gameState.getPlayerTwoScore();
-    scoreTiesEl.textContent = gameState.getTieCount();
-  }
-
-  function boardClickHandler(e) {
-    if (!e.target.dataset.cell) return;
-    game.playTurn(e.target.dataset.cell);
-    updateScreen();
-    if (gameState.getView() === "win") {
-      roundModalEl.showModal();
-      msgWinnerEl.textContent = `${game.getActivePlayer().name} WINS!`;
-      msgRoundEl.innerHTML = `${getPlayerIcon(game.getActivePlayer().token)} TAKES THE ROUND`;
-    } else if (gameState.getView() === "tie") {
-      roundModalEl.showModal();
-      msgWinnerEl.style.display = "none";
-      msgRoundEl.textContent = "ROUND TIED";
-      msgRoundEl.classList.add("round-tied");
-    }
-  }
-
-  function nextRoundClickHandler() {
-    roundModalEl.close();
-    gameState.setView("active");
-    game = GameController(gameState);
-    updateScreen();
-  }
-
-  function quitBtnHandler() {
-    roundModalEl.close();
-    boardEl.removeEventListener("click", boardClickHandler);
-  }
-
-  boardEl.addEventListener("click", boardClickHandler);
-  nextRoundBtn.addEventListener("click", nextRoundClickHandler);
-  restartModalBtn.addEventListener("click", () => restartModalEl.showModal());
-  quitBtn.addEventListener("click", quitBtnHandler);
-
-  updateScreen();
-}
-
-function switchView() {
-  const $ = (selector) => document.querySelector(selector);
-  const setupViewEl = $(".setup-view");
-  const gameViewEl = $(".game-view");
-
-  setupViewEl.toggleAttribute("hidden");
-  gameViewEl.toggleAttribute("hidden");
-}
-
-function newGame() {
-  const $ = (selector) => document.querySelector(selector);
-  const playerXNameInput = $("#player-x-name");
-  const playerONameInput = $("#player-o-name");
-
-  let gameState = GameState();
-  gameState.setPlayerOneName(playerXNameInput.value || "Player X");
-  gameState.setPlayerTwoName(playerONameInput.value || "Player O");
-
-  let game = GameController(gameState);
-
-  switchView();
-  displayController(game, gameState);
-}
-
-function restartGame() {
-  const $ = (selector) => document.querySelector(selector);
-  const restartModalEl = $(".restart-modal");
-  restartModalEl.close();
-  switchView();
-}
-
-function start() {
-  const newGameBtn = document.querySelector(".new-game");
-  const restartBtn = document.querySelector(".restart-modal .affirm");
-  restartBtn.addEventListener("click", restartGame);
-  newGameBtn.addEventListener("click", newGame);
-}
-
-start();
-*/
